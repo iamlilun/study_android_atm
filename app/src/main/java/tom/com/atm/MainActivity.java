8cf4f0de396992bc1750c8b2366fe199084a4210
Lilun <iamlilun@taijun.net>
2018-04-22 18:01:45 +0800

    add menu

--- app/src/main/java/tom/com/atm/MainActivity.java
+++ app/src/main/java/tom/com/atm/MainActivity.java
@@ -1,9 +1,13 @@
 package tom.com.atm;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -12,9 +16,23 @@ public class MainActivity extends AppCompatActivity {
     public static final int RC_LOGIN = 1;
 
     @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_login);
+        EditText edUserid = findViewById(R.id.ed_userid);
+        SharedPreferences setting = getSharedPreferences("atm", MODE_PRIVATE);
+        edUserid.setText(setting.getString("PREF_USERID", ""));
         startActivity(new Intent(this, TestActivity.class));
         if(!login){ //如未登錄，則開啟LoginActivity
             Intent intent = new Intent(this, LoginActivity.class);
